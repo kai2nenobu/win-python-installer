f29a123c87b43928c9e8ec23a909fa39d3bd5591
Tsunenobu Kai <kai2nenobu@gmail.com>
2021-02-23 03:58:00 +0900

    インストーラーのビルド処理をpowershellスクリプトにまとめる

---
+++ ci/build_installer.ps1
@@ -0,0 +1,28 @@
+﻿
+[CmdletBinding()]
+Param(
+  [Parameter(Position=0)]
+  [string]$Ref='master',
+  [string]$OutDirectory
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+if (-not $OutDirectory) {
+  $OutDirectory = Join-Path $PWD 'dist'
+}
+
+# Clone source
+git clone --depth 1 --branch $Ref 'https://github.com/python/cpython.git' cpython
+
+# Monkey patch for python 3.6.x
+if ($Ref.StartsWith('3.6') -or $Ref.StartsWith('v3.6')) {
+  'Monkey Patch for python 3.6.x'
+  $file = 'cpython\Doc\make.bat'
+  ((Get-Content -Path $file -Raw) -replace '-m pip install sphinx','-m pip install sphinx==2.2.0') `
+    | Set-Content -Path $file
+}
+
+# Build installer
+cmd /c cpython\Tools\msi\buildrelease.bat -x64 --skip-pgo --skip-nuget --skip-zip -o $OutDirectory
