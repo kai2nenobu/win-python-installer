05943229da63e60bd0185cd19a40e8f151fee814
Tsunenobu Kai <kai2nenobu@gmail.com>
2021-03-01 13:14:46 +0900

    python3.6.xのときだけPGOを無効にする
    ビルド設定が良くないのか、3.6.xだとPGOコンパイルに失敗するため。

--- ci/build_installer.ps1
+++ ci/build_installer.ps1
@@ -13,18 +13,23 @@ if (-not $OutDirectory) {
   $OutDirectory = Join-Path $PWD 'dist'
 }
 
+$BuildOptions = @('-x64','--skip-nuget','--skip-zip','-o',"$OutDirectory")
+
 # Clone source
 git clone --depth 1 --branch $Ref 'https://github.com/python/cpython.git' cpython
 if ($LASTEXITCODE -gt 0) { exit $LASTEXITCODE }
 
-# Monkey patch for python 3.6.x
+# Special operations for version 3.6.x
 if ($Ref.StartsWith('3.6') -or $Ref.StartsWith('v3.6')) {
   'Monkey Patch for python 3.6.x'
   $file = 'cpython\Doc\make.bat'
   ((Get-Content -Path $file -Raw) -replace '-m pip install sphinx','-m pip install sphinx==2.2.0') `
     | Set-Content -Path $file
+  'Skip PGO compile for python 3.6.x'
+  $BuildOptions += '--skip-pgo'
 }
 
 # Build installer
-cmd /c cpython\Tools\msi\buildrelease.bat -x64 --skip-nuget --skip-zip -o $OutDirectory
+
+cmd /c cpython\Tools\msi\buildrelease.bat @BuildOptions
 if ($LASTEXITCODE -gt 0) { exit $LASTEXITCODE }
