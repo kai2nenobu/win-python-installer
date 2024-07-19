1cefa22e904e3971a70d3a2070921cb182dcf739
Tsunenobu Kai <kai2nenobu@gmail.com>
2024-07-19 16:40:03 +0900

    Generate matrix on scheduled build

--- ci/tag2matrix.py
+++ ci/tag2matrix.py
@@ -1,12 +1,28 @@
 """
-Convert CPython tags to a build matrix for GitHub Actions
+Generate a build matrix from GitHub Actions event
+
+- Daily scheduled build -> all versions
+- release PR -> release target versions in PR title
+- non-release PR -> all versions
+- manual workflow -> versions specified as input
 """
 from __future__ import annotations
-
-
+from dataclasses import dataclass
 import json
-import sys
+import os
 import unittest
+
+
+@dataclass
+class GitHubEvent:
+    EVENT_NAME: str
+    """Event name for GitHub Actions workflow (github.event_name)"""
+
+    BRANCH_NAME: str
+    """Branch name triggers GitHub Actions workflow (github.head_ref or github.ref)"""
+
+    PR_TITLE: str | None = None
+    """Pull Request title for GitHub Actions workflow (github.event.pull_request.title)"""
 
 
 BASE_MATRIX = {
@@ -14,15 +30,24 @@ BASE_MATRIX = {
     "3.9": {"os": "windows-2019"},
     "3.10": {"os": "windows-2019"},
     "3.11": {"os": "windows-2019"},
+    "3.12": {"os": "windows-2019"},
+    "3.13": {"os": "windows-2022", "branch": "main"},
 }
 
-def to_matrix(tags: str) -> list[dict]:
+
+def to_matrix(event: GitHubEvent) -> list[dict]:
+    if event.EVENT_NAME == "schedule":
+        tags = "3.8/3.9/3.10/3.11/3.12/3.13"
+    else:
+        tags = ""
     tag_list = tags.split("/")
 
     def convert(tag: str) -> dict:
         minor_version = ".".join(tag.lstrip("v").split(".")[0:2])
         matrix = BASE_MATRIX[minor_version].copy()
-        matrix["tag"] = tag
+        matrix["version"] = tag
+        if "branch" not in matrix:
+            matrix["branch"] = minor_version
         return matrix
 
     matrix = [convert(tag) for tag in tag_list]
@@ -30,7 +55,12 @@ def to_matrix(tags: str) -> list[dict]:
 
 
 def main() -> None:
-    print(json.dumps(to_matrix(sys.argv[1])))
+    event = GitHubEvent(
+        EVENT_NAME=os.getenv("EVENT_NAME"),
+        BRANCH_NAME=os.getenv("BRANCH_NAME"),
+        PR_TITLE=os.getenv("PR_TITLE")
+    )
+    print(json.dumps(event))
 
 
 if __name__ == "__main__":
@@ -38,31 +68,15 @@ if __name__ == "__main__":
 
 
 class Test(unittest.TestCase):
-    def test_tags(self):
+    def test_schedule_event(self):
         self.assertSequenceEqual(
-            to_matrix("v3.10.14/v3.9.19/v3.8.19/v3.11.10"),
+            to_matrix(GitHubEvent(**{"EVENT_NAME": "schedule", "BRANCH_NAME": "main"})),
             [
-                {"tag": "v3.10.14", "os": "windows-2019"},
-                {"tag": "v3.9.19", "os": "windows-2019"},
-                {"tag": "v3.8.19", "os": "windows-2019", "HOST_PYTHON": "3.8"},
-                {"tag": "v3.11.10", "os": "windows-2019"},
-            ]
-        )
-
-    def test_single_tag(self):
-        self.assertSequenceEqual(
-            to_matrix("v3.8.19"),
-            [
-                {"tag": "v3.8.19", "os": "windows-2019", "HOST_PYTHON": "3.8"},
-            ]
-        )
-
-    def test_branches(self):
-        self.assertSequenceEqual(
-            to_matrix("3.8/3.9/3.10"),
-            [
-                {"tag": "3.8", "os": "windows-2019", "HOST_PYTHON": "3.8"},
-                {"tag": "3.9", "os": "windows-2019"},
-                {"tag": "3.10", "os": "windows-2019"},
+                {"version": "3.8", "os": "windows-2019", "HOST_PYTHON": "3.8", "branch": "3.8"},
+                {"version": "3.9", "os": "windows-2019", "branch": "3.9"},
+                {"version": "3.10", "os": "windows-2019", "branch": "3.10"},
+                {"version": "3.11", "os": "windows-2019", "branch": "3.11"},
+                {"version": "3.12", "os": "windows-2019", "branch": "3.12"},
+                {"version": "3.13", "os": "windows-2022", "branch": "main"},
             ]
         )
