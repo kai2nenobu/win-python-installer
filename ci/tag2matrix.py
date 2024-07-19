a4f975fbdfaf6de912f10d2abe26be1e69d82810
Tsunenobu Kai <kai2nenobu@gmail.com>
2024-07-19 16:48:16 +0900

    Generate matrix on pull request event

--- ci/tag2matrix.py
+++ ci/tag2matrix.py
@@ -10,6 +10,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 import json
 import os
+import re
 import unittest
 
 
@@ -33,11 +34,17 @@ BASE_MATRIX = {
     "3.12": {"os": "windows-2019"},
     "3.13": {"os": "windows-2022", "branch": "main"},
 }
+ALL_VERSIONS = "/".join(BASE_MATRIX.keys())
 
 
 def to_matrix(event: GitHubEvent) -> list[dict]:
     if event.EVENT_NAME == "schedule":
-        tags = "3.8/3.9/3.10/3.11/3.12/3.13"
+        tags = ALL_VERSIONS
+    elif event.EVENT_NAME == "pull_request":
+        if event.BRANCH_NAME == "new_release":
+            tags = re.sub(r"^.*New release *", "", event.PR_TITLE)
+        else:
+            tags = ALL_VERSIONS
     else:
         tags = ""
     tag_list = tags.split("/")
@@ -71,6 +78,29 @@ class Test(unittest.TestCase):
     def test_schedule_event(self):
         self.assertSequenceEqual(
             to_matrix(GitHubEvent(**{"EVENT_NAME": "schedule", "BRANCH_NAME": "main"})),
+            [
+                {"version": "3.8", "os": "windows-2019", "HOST_PYTHON": "3.8", "branch": "3.8"},
+                {"version": "3.9", "os": "windows-2019", "branch": "3.9"},
+                {"version": "3.10", "os": "windows-2019", "branch": "3.10"},
+                {"version": "3.11", "os": "windows-2019", "branch": "3.11"},
+                {"version": "3.12", "os": "windows-2019", "branch": "3.12"},
+                {"version": "3.13", "os": "windows-2022", "branch": "main"},
+            ]
+        )
+
+    def test_release_pr(self):
+        self.assertSequenceEqual(
+            to_matrix(GitHubEvent(**{"EVENT_NAME": "pull_request", "BRANCH_NAME": "new_release", "PR_TITLE": "✨New release v3.10.14/v3.9.19/v3.8.19"})),
+            [
+                {"version": "v3.10.14", "os": "windows-2019", "branch": "3.10"},
+                {"version": "v3.9.19", "os": "windows-2019", "branch": "3.9"},
+                {"version": "v3.8.19", "os": "windows-2019", "HOST_PYTHON": "3.8", "branch": "3.8"},
+            ]
+        )
+
+    def test_non_release_pr(self):
+        self.assertSequenceEqual(
+            to_matrix(GitHubEvent(**{"EVENT_NAME": "pull_request", "BRANCH_NAME": "feature_branch", "PR_TITLE": "Some feature"})),
             [
                 {"version": "3.8", "os": "windows-2019", "HOST_PYTHON": "3.8", "branch": "3.8"},
                 {"version": "3.9", "os": "windows-2019", "branch": "3.9"},
